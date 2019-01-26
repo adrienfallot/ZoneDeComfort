f5c4be9aed5b6a712581e64c96179086d45cb472
Adrien FALLOT <adrien.fallot@yahoo.fr>
2019-01-27 00:00:46 +0100

    TextBulle structure

---
+++ Assets/Scripts/NPCTalk.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCTalk : MonoBehaviour
+{
+	public Animator bulle;
+	public Text textBulle;
+
+	[System.Serializable]
+	public struct Day
+	{
+		[Multiline]
+		public string greeting;
+        public Dialogue[] dialogue;
+		[Multiline]
+		public string bye;
+	}
+
+	[System.Serializable]
+	public struct Dialogue
+	{
+		[Multiline]
+        public string text;
+	}
+
+	[Header("Texts")]
+	public Day[] days;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+		if (other.CompareTag("Player"))
+		{
+			textBulle.text = days[DaysManager.dayNumber].greeting;
+			bulle.Play("Pop");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+    {
+		if (other.CompareTag("Player"))
+		{
+			bulle.Play("Unpop");
+		}
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     private Rigidbody2D rigidBody2D;
-    private bool moving = false;
 
 	[Header("Sprite")]
 	public Animator animator;
@@ -23,8 +22,6 @@ public class PlayerController : MonoBehaviour
         Vector2 move = Vector2.zero;
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
-
-        print(move);
 
         rigidBody2D.velocity = move * speed;
 
