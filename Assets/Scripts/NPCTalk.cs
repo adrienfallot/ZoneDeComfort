94a15ca7edbfe6a83cde2083e7f7fe3caa341c03
Adrien FALLOT <adrien.fallot@yahoo.fr>
2019-01-27 02:09:05 +0100

    Talk system

--- Assets/Scripts/NPCTalk.cs
+++ Assets/Scripts/NPCTalk.cs
@@ -8,12 +8,19 @@ public class NPCTalk : MonoBehaviour
 	public Animator bulle;
 	public Text textBulle;
 
+	private bool isTalking = false;
+	private bool bye = false;
+	private bool bullePop = false;
+
+	private int numberText = 0;
+
 	[System.Serializable]
 	public struct Day
 	{
 		[Multiline]
 		public string greeting;
-        public Dialogue[] dialogue;
+		[Multiline]
+        public string[] dialogue;
 		[Multiline]
 		public string bye;
 	}
@@ -33,10 +40,16 @@ public class NPCTalk : MonoBehaviour
     {
 		if (other.CompareTag("Player"))
 		{
-			AkSoundEngine.PostEvent("Env_Greet_Nice", this.gameObject); //TODO: comfort/discomfort
+			if(!bye)
+			{
+				AkSoundEngine.PostEvent("Env_Greet_Nice", this.gameObject); //TODO: comfort/discomfort
 
-			textBulle.text = days[DaysManager.dayNumber].greeting;
-			bulle.Play("Pop");
+				textBulle.text = days[DaysManager.dayNumber].greeting;
+				bulle.Play("Pop");
+				bullePop = true;
+			}
+			
+			isTalking = true;
 		}
 	}
 
@@ -45,7 +58,49 @@ public class NPCTalk : MonoBehaviour
 		if (other.CompareTag("Player"))
 		{
 			bulle.Play("Unpop");
+
+			isTalking = false;
+			bullePop = false;
+
+			if(!bye)
+			{
+				numberText = 0;
+			}
 		}
     }
+
+	 // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetButtonDown("Interact") && isTalking)
+		{
+			if (!bye)
+			{
+				textBulle.text = days[DaysManager.dayNumber].dialogue[numberText];
+				numberText ++;
+				bye = days[DaysManager.dayNumber].dialogue.Length == numberText;
+			}
+			else
+			{
+				textBulle.text = days[DaysManager.dayNumber].bye;
+
+				if (!bullePop)
+				{
+					bulle.Play("Pop");
+					bullePop = true;
+				}
+			}
+		}
+	}
+
+	public void FinishDay()
+	{
+		bulle.Play("Unpop");
+
+		isTalking = false;
+		bullePop = false;
+		bye = false;
+		numberText = 0;
+	}
 
 }
