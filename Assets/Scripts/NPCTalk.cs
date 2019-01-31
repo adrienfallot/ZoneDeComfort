ca5d4855b623c029ca0deea22264b7ab4c787443
Nicolas Noël <nico@krismas.net>
2019-01-31 13:43:40 +0100

    Polsh_Collider, debug croissant en cours

--- Assets/Scripts/NPCTalk.cs
+++ Assets/Scripts/NPCTalk.cs
@@ -5,147 +5,151 @@ using UnityEngine.UI;
 
 public class NPCTalk : MonoBehaviour
 {
-	public Animator bulle;
-	public Text textBulle;
-	public Animator NPCAnimator;
+    public Animator bulle;
+    public Text textBulle;
+    public Animator NPCAnimator;
 
-	public ComfortZone[] comfortZones;
+    public ComfortZone[] comfortZones;
 
-	private bool isTalking = false;
-	private bool bye = false;
-	private bool bullePop = false;
+    private bool isTalking = false;
+    private bool bye = false;
+    private bool bullePop = false;
 
-	private int numberText = 0;
-	private int numberDialogue = 0;
+    private int numberText = 0;
+    private int numberDialogue = 0;
 
-	public static List<NPCTalk> NPC;
+    public static List<NPCTalk> NPC;
 
-	[System.Serializable]
-	public struct Day
-	{
-		public bool hasACroissant;
+    [System.Serializable]
+    public struct Day
+    {
+        public bool hasACroissant;
 
-		[Multiline]
-		public string greeting;
-		[Multiline]
+        [Multiline]
+        public string greeting;
+        [Multiline]
         public string[] dialogue;
-		[Multiline]
-		public string bye;
-	}
+        [Multiline]
+        public string bye;
+    }
 
-	[System.Serializable]
-	public struct Dialogue
-	{
-		[Multiline]
+    [System.Serializable]
+    public struct Dialogue
+    {
+        [Multiline]
         public string text;
-	}
+    }
 
-	[Header("Texts")]
-	public Day[] days;
+    [Header("Texts")]
+    public Day[] days;
 
 
     void OnTriggerEnter2D(Collider2D other)
     {
-		if (other.CompareTag("Player"))
-		{
-			if(!bye)
-			{
-				AkSoundEngine.PostEvent("Env_Greet_Nice", this.gameObject); //TODO: comfort/discomfort
+        if (other.CompareTag("Player"))
+        {
+            if (!bye)
+            {
+                AkSoundEngine.PostEvent("Env_Greet_Nice", this.gameObject); //TODO: comfort/discomfort
 
-				textBulle.text = days[numberDialogue].greeting;
-				bulle.Play("Pop");
-				bullePop = true;
-			}
-			
-			isTalking = true;
-		}
-	}
+                textBulle.text = days[numberDialogue].greeting;
+                bulle.Play("Pop");
+                bullePop = true;
+            }
 
-	void OnTriggerExit2D(Collider2D other)
-    {
-		if (other.CompareTag("Player"))
-		{
-			bulle.Play("Unpop");
-
-			isTalking = false;
-			bullePop = false;
-
-			if(!bye)
-			{
-				numberText = 0;
-			}
-		}
+            isTalking = true;
+        }
     }
 
-	void Awake()
-	{
-		if(NPC == null){
-			NPC = new List<NPCTalk>();
-		}
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            bulle.Play("Unpop");
 
-		NPC.Add(this);
+            isTalking = false;
+            bullePop = false;
 
-		NPCAnimator.SetBool("Walking", days[numberDialogue].hasACroissant); //croissant
-	}
+            if (!bye)
+            {
+                numberText = 0;
+            }
+        }
+    }
 
-	// Update is called once per frame
+    void Awake()
+    {
+        if (NPC == null)
+        {
+            NPC = new List<NPCTalk>();
+        }
+
+        NPC.Add(this);
+
+        NPCAnimator.SetBool("Walking", days[numberDialogue].hasACroissant); //croissant
+    }
+
+    // Update is called once per frame
     void Update()
     {
-		if (Input.GetButtonDown("Interact") && isTalking)
-		{
-			Day today = days[numberDialogue];
+        if (Input.GetButtonDown("Interact") && isTalking)
+        {
+            Day today = days[numberDialogue];
 
-			if (!bye)
-			{
-				if (today.dialogue.Length == 0)
-				{
-					textBulle.text = today.greeting;
-				}
-				else
-				{
-					textBulle.text = today.dialogue[numberText];
-					numberText ++;
-					bye = today.dialogue.Length == numberText;
-				}
-			}
-			else
-			{
-				if (today.hasACroissant){
-					today.hasACroissant = false;
-					NPCAnimator.SetBool("Walking", false); //croissant
-					PlayerController.croissant = true;
-					AkSoundEngine.PostEvent("P_Success", gameObject);
-				}
+            if (!bye)
+            {
+                if (today.dialogue.Length == 0)
+                {
+                    textBulle.text = today.greeting;
+                }
+                else
+                {
+                    textBulle.text = today.dialogue[numberText];
+                    numberText++;
+                    bye = today.dialogue.Length == numberText;
+                }
+            }
+            else
+            {
+                if (today.hasACroissant)
+                {
+                    Debug.Log("Before" + today.hasACroissant);
+                    today.hasACroissant = false;
+                    Debug.Log("After" + today.hasACroissant);
+                    NPCAnimator.SetBool("Walking", false); //croissant
+                    PlayerController.croissant = true;
+                    AkSoundEngine.PostEvent("P_Success", gameObject);
+                }
 
-				textBulle.text = today.bye;
+                textBulle.text = today.bye;
 
-				if (!bullePop)
-				{
-					bulle.Play("Pop");
-					bullePop = true;
-				}
-			}
-		}
-	}
+                if (!bullePop)
+                {
+                    bulle.Play("Pop");
+                    bullePop = true;
+                }
+            }
+        }
+    }
 
-	public void FinishDay()
-	{
-		bulle.Play("Unpop");
+    public void FinishDay()
+    {
+        bulle.Play("Unpop");
 
-		if (bye)
-		{
-			numberDialogue++;
-		}
+        if (bye)
+        {
+            numberDialogue++;
+        }
 
-		isTalking = false;
-		bullePop = false;
-		bye = false;
-		numberText = 0;
+        isTalking = false;
+        bullePop = false;
+        bye = false;
+        numberText = 0;
 
-		foreach (var comfortZone in comfortZones)
-		{
-			comfortZone.Expend();
-		} 
-	}
+        foreach (var comfortZone in comfortZones)
+        {
+            comfortZone.Expend();
+        }
+    }
 
 }
